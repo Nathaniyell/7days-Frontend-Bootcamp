9a62a0b525083f2b06eb777c56720599028298af
Nathaniyell <essien.nathan@yahoo.com>
2023-09-11 21:01:30 +0100

    add logic to help with the song search

---
+++ Kanye Lyrics App/app.js
@@ -0,0 +1,9 @@
+let inputValue;
+const inputField = document.getElementById("inputfield");
+inputField.addEventListener("change", (e) => {
+  console.log(e.target.value);
+  inputValue = e.target.value;
+});
+
+
+
