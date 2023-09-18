0cb2f40658f0f32104ac8f1a7102c89daa5cda90
Nathaniyell <essien.nathan@yahoo.com>
2023-09-18 21:20:53 +0100

    Remove javascript functionality

--- Kanye Lyrics App/app.js
+++ Kanye Lyrics App/app.js
@@ -1,9 +1,9 @@
-let inputValue;
-const inputField = document.getElementById("inputfield");
-inputField.addEventListener("change", (e) => {
-  console.log(e.target.value);
-  inputValue = e.target.value;
-});
+// let inputValue;
+// const inputField = document.getElementById("inputfield");
+// inputField.addEventListener("change", (e) => {
+//   console.log(e.target.value);
+//   inputValue = e.target.value;
+// });
 
 
 
